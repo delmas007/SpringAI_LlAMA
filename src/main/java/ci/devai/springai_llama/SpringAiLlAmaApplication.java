1a75ef2c231935de76fb98395c94ba947b296373
delmas <azertyazerty1ze@gmail.com>
2024-11-30 22:40:12 +0000

    init

---
+++ src/main/java/ci/devai/springai_llama/SpringAiLlAmaApplication.java
@@ -0,0 +1,13 @@
+package ci.devai.springai_llama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiLlAmaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiLlAmaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ci/devai/springai_llama/SpringAiLlAmaApplicationTests.java
@@ -0,0 +1,13 @@
+package ci.devai.springai_llama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiLlAmaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
